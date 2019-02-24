e061a2c1d0bbaf731daa6a42f924e9476bfdfab1
kamalm100 <47140792+kamalm100@users.noreply.github.com>
2019-02-24 20:30:55 +0000

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -2,5 +2,11 @@ class profile::base {
   user {'admin':
     ensure => present
   }
+  
+  file {'/root/server.info':
+     ensure  => present,
+     content => $fqdn,
+  }
+  
   include profile::ssh_server
 }
