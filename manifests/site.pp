b356d4bf3ce356fb9470ad871724548863d00f87
kamalm100 <47140792+kamalm100@users.noreply.github.com>
2019-02-25 16:36:18 +0000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,12 +7,16 @@ node default {
   
 node 'minecraft.home.com' {
   include role::minecraft_server
+  include profile::ssh_server
+
   }
 
 node /^web/ {
   include role::app_server
+  include profile::ssh_server
   }
 
 node /^db/ {
   include role::db_server
+  include profile::ssh_server
   }
