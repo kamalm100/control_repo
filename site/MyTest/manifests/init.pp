0be31fc90248fca9cc9a369194c5a1fa1f8fb586
kamalm100 <47140792+kamalm100@users.noreply.github.com>
2019-02-25 18:22:16 +0000

    Update init.pp

--- site/MyTest/manifests/init.pp
+++ site/MyTest/manifests/init.pp
@@ -1,1 +1,8 @@
-class MyTest {}
+class MyTest {
+  file {'/root/test':
+    ensure => directory,
+    }
+  file {'/root/test/hello.txt':
+    ensure => present,
+    content => 'this is ${fqdn}',
+}
